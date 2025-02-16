a866f6ee79559a1cb46b3de2de1f0b1b9f877419
Mikhail Denisov <denisovmvtmb@yandex.ru>
2025-02-16 13:52:59 +0500

    correct profile model (excluded redundant user_id attribute which was already created by Mixin UserRealationMixin

--- core/models/profile.py
+++ core/models/profile.py
@@ -14,5 +14,3 @@ class Profile(Base, UserRelationMixin):
     first_name: Mapped[str | None] = mapped_column(String(40))
     last_name: Mapped[str | None] = mapped_column(String(40))
     bio: Mapped[str | None]
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
